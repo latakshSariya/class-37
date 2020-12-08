af9721ace33eaa8d0cbcbbb95ec2d3d12504589d
latakshSariya <72252422+latakshSariya@users.noreply.github.com>
2020-12-08 13:59:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var database;
+var GameState = 0;
+var PlayerCount;
+var Form1;
+var players;
+var PlayersInfo;
+var game;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    game = new gameObject();
+    game.getState();
+    game.start();
+  
+}
+
+function draw(){
+
+}
